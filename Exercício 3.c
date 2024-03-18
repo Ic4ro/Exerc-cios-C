d8aed384f9d7d0e101e78b7aebde95858e25e04b
Ic4ro <161033248+Ic4ro@users.noreply.github.com>
2024-03-18 09:43:42 -0300

    Add files via upload

---
+++ Exercício 3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){ 
+	setlocale(LC_ALL,"");
+	
+	int numero;
+	
+	printf("Digite um Número: ");
+	scanf("%i",&numero);
+	
+	printf("\n=== Exibindo Resultados ===\n");
+	
+	if (numero % 2 == 0) {
+		printf("Par.");
+	} else {
+		printf("Ímpar.");
+	}
+	
+	return 0;
+}
+
