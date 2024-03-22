ddf87d519efd6c497cadde761c9998f5bd8e533f
Ic4ro <161033248+Ic4ro@users.noreply.github.com>
2024-03-22 11:04:13 -0300

    Exercício 08.c

--- Exercício 08.c
+++ Exercício 08.c
@@ -6,20 +6,20 @@
 int main(){ 
 	setlocale(LC_ALL,"");
 	
-	//Declarando Vari·veis.
+	//Declarando Vari√°veis.
 	char loginSalvo[200] = "Marta";
 	char senhaSalva[200] = "123";
 	char login[200];
 	char senha[200];
 	
-	//Solicitando Login e Senha do Usu·rio.
+	//Solicitando Login e Senha do Usu√°rio.
 	printf("Digite Seu Login: ");
 	scanf("%s",&login);
 	
 	printf("Digite Sua Senha:  ");
 	scanf("%s",&senha);
 	
-	//Verificando Dados Do Usu·rio.
+	//Verificando Dados Do Usu√°rio.
 	if (strcmp(login, loginSalvo) == 0 && strcmp(senha, senhaSalva) == 0) {
 		printf("Bem Vindo!");
 	} else {
